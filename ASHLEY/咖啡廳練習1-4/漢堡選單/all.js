09042cf236987cc5021099bb48deea3f5a24adee
reginaachung <regina87721@gmail.com>
2023-12-11 17:10:18 +0800

    12.11

---
+++ ASHLEY/咖啡廳練習1-4/漢堡選單/all.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.menuSwitch').click(function () {
+        $('.menu').toggleClass('open');
+        // // console.log('123');
+        // if ($('.menu').hasClass('open')) {
+        //     $('.menu').removeClass('open');
+        // } else {
+        //     $('.menu').addClass('open');
+        // }
+    })
+})
